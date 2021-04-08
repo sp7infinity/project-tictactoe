5967f448d6ceb5bc66ecdc240ef929470750aa60
Subhasish <pandasonu589@gmail.com>
2021-04-08 11:54:03 +0530

    Added markup and styling for game-board

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-const clickPlay = (() => {
+const playGame = (() => {
     const namePlayerOne = document.getElementById(player1);
     const namePlayerTwo = document.getElementById(player2);
-})
+    console.log(namePlayerOne.value);
 
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (namePlayerOne.value !== '' && namePlayerTwo.value !== '') {
+        //   startGame();
+          form.classList.add('hidden');
+          document.querySelector('.place').classList.remove('hidden');
+        } else {
+          window.location.reload();
+        }
+    });
+})
